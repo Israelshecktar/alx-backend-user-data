4235c001717cf03621c3e855fa77c4fc1ee00055
Israelshecktar <iamshecktar1996@gmail.com>
2024-06-10 16:11:57 +0000

    added basemodel db

---
+++ user.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""User model for sqlalchemy"""
+
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+Base = declarative_base()
+
+
+class User(Base):
+    """User class representing a user in the database"""
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
+
+    # set up the database engine to interact with the database
+    engine = create_engine('sqlite:///user.auth.db')
+
+    # create all the tabkes in the database
+    Base.metadata.create_all(engine)
+
+    # Create a configured "Session" class
+    Session = sessionmaker(bind=engine)
