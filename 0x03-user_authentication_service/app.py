d368297df5456d1c72d9e6e206ba8ea03d9450de
Israelshecktar <iamshecktar1996@gmail.com>
2024-06-13 19:07:32 +0000

    fixed to 9ep8

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -5,10 +5,12 @@ from flask import Flask, Request, jsonify
 
 app = Flask(__name__)
 
+
 @app.route("/", methods=["GET"])
 def home():
     """Return a JSON paylaod with a welcome message"""
     return jsonify({"message": "Bienvenue"})
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
