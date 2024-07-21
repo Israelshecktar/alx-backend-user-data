4b2994ec0b5acbe598e32c3a01e023df58f49443
Israelshecktar <iamshecktar1996@gmail.com>
2024-07-21 18:54:05 +0000

    refactor to pass lint

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,10 +8,12 @@ import base64
 
 
 class BasicAuth(Auth):
-    """BasicAuth class"""
+    """ BasicAuth class"""
 
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
-        """Extract base64 authorization header"""
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """ Extract base64 authorization header
+        """
         if authorization_header is None:
             return None
         if not isinstance(authorization_header, str):
@@ -21,57 +23,61 @@ class BasicAuth(Auth):
         return authorization_header[6:]
 
     def decode_base64_authorization_header(
-        self, base64_authorization_header: str
-    ) -> str:
-        """Decode base64 authorization header"""
+            self, base64_authorization_header: str) -> str:
+        """ Decode base64 authorization header
+        """
         if base64_authorization_header is None:
             return None
         if not isinstance(base64_authorization_header, str):
             return None
         try:
-            base64_bytes = base64_authorization_header.encode("utf-8")
+            base64_bytes = base64_authorization_header.encode('utf-8')
             message_bytes = base64.b64decode(base64_bytes)
-            message = message_bytes.decode("utf-8")
+            message = message_bytes.decode('utf-8')
             return message
         except Exception:
             return None
 
     def extract_user_credentials(
-        self, decoded_base64_authorization_header: str
-    ) -> (str, str):
-        """Extract user credentials"""
+            self, decoded_base64_authorization_header: str) -> (str, str):
+        """ Extract user credentials
+        """
         if decoded_base64_authorization_header is None:
             return None, None
         if not isinstance(decoded_base64_authorization_header, str):
             return None, None
-        if ":" not in decoded_base64_authorization_header:
+        if ':' not in decoded_base64_authorization_header:
             return None, None
-        credentials = decoded_base64_authorization_header.split(":", 1)
+        credentials = decoded_base64_authorization_header.split(':', 1)
         return credentials[0], credentials[1]
 
-    def user_object_from_credentials(self, user_email: str, user_pwd: str) -> dict:
-        """User object from credentials"""
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> dict:
+        """ User object from credentials
+        """
         if user_email is None or not isinstance(user_email, str):
             return None
         if user_pwd is None or not isinstance(user_pwd, str):
             return None
         try:
-            users = User.search({"email": user_email})
+            users = User.search({'email': user_email})
         except Exception:
             return None
         for user in users:
             if user.is_valid_password(user_pwd):
                 return user
 
-    def current_user(self, request=None) -> TypeVar("User"):
-        """Current user"""
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Current user
+        """
         try:
             auth_header = self.authorization_header(request)
-            base64_header = self.extract_base64_authorization_header(auth_header)
+            base64_header = self.extract_base64_authorization_header(
+                auth_header)
             decoded_base64_header = self.decode_base64_authorization_header(
-                base64_header
-            )
-            user_email, user_pwd = self.extract_user_credentials(decoded_base64_header)
+                base64_header)
+            user_email, user_pwd = self.extract_user_credentials(
+                decoded_base64_header)
             user = self.user_object_from_credentials(user_email, user_pwd)
             return user
         except Exception:
